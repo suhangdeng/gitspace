8b912e07becbb16a11e19a4ffe273075c74e0a6c
suhangdeng <592613480@qq.com>
2019-12-07 15:21:02 +0800

    Spring-Boot-Validate第三次提交，启动类上加了句hello

--- Spring-Boot-Validate/src/main/java/com/bjsxt/MyApplication.java
+++ Spring-Boot-Validate/src/main/java/com/bjsxt/MyApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+        System.out.println("hello");
 
     }
 }
